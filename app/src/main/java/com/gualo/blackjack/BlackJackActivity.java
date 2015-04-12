aef0612d33924bfe2e25c8f53a525caa9c69332b
kachyto <albertogrca@gmail.com>
2015-04-12 18:56:24 +0200

    commit

--- app/src/main/java/com/gualo/blackjack/BlackJackActivity.java
+++ app/src/main/java/com/gualo/blackjack/BlackJackActivity.java
@@ -10,8 +10,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.gualo.blackjack.domain.BlackJack;
+import com.gualo.blackjack.jsonMessages.BlackJackBoardMessage;
+import com.gualo.blackjack.jsonMessages.BlackJackMatchReadyMessage;
+import com.gualo.blackjack.jsonMessages.BlackJackWaitingMessage;
 import com.maco.tresenraya.R;
-import com.maco.tresenraya.jsonMessages.TresEnRayaMovement;
+
 import com.gualo.blackjack.jsonMessages.BlackJackMovement;
 
 import org.json.JSONException;
@@ -19,6 +22,10 @@ import org.json.JSONObject;
 
 import edu.uclm.esi.common.androidClient.dialogs.Dialogs;
 import edu.uclm.esi.common.androidClient.domain.Store;
+import edu.uclm.esi.common.androidClient.http.Proxy;
+import edu.uclm.esi.common.jsonMessages.ErrorMessage;
+import edu.uclm.esi.common.jsonMessages.JSONMessage;
+import edu.uclm.esi.common.jsonMessages.JSONParameter;
 
 public class BlackJackActivity extends ActionBarActivity {
     private BlackJack match;
@@ -79,8 +86,46 @@ public class BlackJackActivity extends ActionBarActivity {
 
     this.match=new BlackJack(this);
     }
+    private void loadMatch() {
+        Store store=Store.get();
+        this.tvPlayer.setText(store.getUser().getEmail());
+        JSONParameter jspIdUser=new JSONParameter("idUser", ""+store.getUser().getId());
+        JSONParameter jspIdGame=new JSONParameter("idGame", ""+store.getIdGame());
+        JSONParameter jspIdMatch=new JSONParameter("idMatch", ""+store.getIdMatch());
+        try {
+            JSONMessage jsm= Proxy.get().postJSONOrderWithResponse("GetBoard.action", jspIdUser, jspIdGame, jspIdMatch);
+            if (jsm.getType().equals(BlackJackBoardMessage.class.getSimpleName())) {
+                loadBoard(jsm);
+            } else {
+                ErrorMessage em=(ErrorMessage) jsm;
+                Toast.makeText(this, em.getText(), Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
 
-
+    public void loadBoard(JSONMessage jsm) throws JSONException {
+        BlackJackBoardMessage board=(BlackJackBoardMessage) jsm;
+        if (this.match==null)
+            this.match=new BlackJack(this);
+        this.match.load(board);
+    }
+    public void loadMessage(BlackJackWaitingMessage bj) {
+        this.tvMessage.setText(bj.getText());
+    }
+    public void loadReadyMessage(BlackJackMatchReadyMessage rm) {
+        String player1=rm.getPlayer1();
+        String player2=rm.getPlayer2();
+        String opponent;
+        if (Store.get().getUser().getEmail().equals(player1)) {
+            opponent = player2;
+            this.tvOpponent.setText(opponent);
+        }else
+            opponent=player1;
+            this.tvOpponent.setText(opponent);
+        this.tvOpponent.setText("Playing against " + opponent);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- app/src/main/java/edu/uclm/esi/common/androidClient/http/Proxy.java
+++ app/src/main/java/edu/uclm/esi/common/androidClient/http/Proxy.java
@@ -14,7 +14,7 @@ public class Proxy {
 	private String urlServer;
 
 	private Proxy() {
-		this.urlServer = "192.168.1.14:8080";
+		this.urlServer = "192.168.1.33:8080";
 	}
 
 	public static Proxy get() {
